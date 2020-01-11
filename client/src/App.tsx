09c98db4fa599b039137352a588a06a8374b27fe
mahume <michael.a.hume@gmail.com>
2020-01-10 19:27:20 -0700

    Setting up CRA with TS

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      values: [
+        { id: 1, name: "Value 1" },
+        { id: 2, name: "Value 2" }
+      ]
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
